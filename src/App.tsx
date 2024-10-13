9754544466237dad52e5b0530b03290e932576cb
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-10-13 22:45:08 +0700

    update routes with ScreenNameConfig, structure for detail

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ProductListScreen from "./views/screens/ProductList/index";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./views/screens/Detail/detail";
+import ScreenNameConfig from "./configs/ScreenNameConfig";
 
 function App() {
   return (
     <>
-        <Router>
-            <Routes>
-                <Route path="/about" element={<Detail />} />
-                <Route path="/" element={<ProductListScreen />} />
-            </Routes>
-        </Router>
+      <Router>
+        <Routes>
+          <Route path={ScreenNameConfig.HOME} element={<ProductListScreen />} />
+          <Route path={ScreenNameConfig.DETAIL} element={<Detail />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- src/views/screens/Detail/detail.tsx
+++ src/views/screens/Detail/detail.tsx
@@ -1,7 +1,35 @@
-export default  function () {
-    return (
-        <div>
-            Detail
-        </div>
-    )
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import RootLayout from "../../layouts/RootLayout";
+import { Col, Container, Row } from "react-bootstrap";
+
+export default function DetailScreen() {
+  //contexts
+  const location = useLocation();
+
+  //states
+  const productId: number = location.state ?? -1;
+
+  //useEffect
+  useEffect(() => {
+    console.log(productId);
+  }, []);
+
+  return (
+    <RootLayout>
+      <Container>
+        {/* common information */}
+        <Row>
+          {/* image carousel */}
+          <Col md={{ span: 6 }}></Col>
+
+          {/* image size, brand, ... */}
+          <Col md={{ span: 6 }}></Col>
+        </Row>
+
+        {/* detail description */}
+        <Row></Row>
+      </Container>
+    </RootLayout>
+  );
 }
