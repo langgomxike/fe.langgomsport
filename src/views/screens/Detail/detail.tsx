26957a1cf2e5277ab8e8ede6043410fac0680ca2
VanDuPlus <22211TT1357@mail.tdc.edu.vn>
2024-11-15 20:02:13 +0700

    footer
    # Conflicts:
    #	src/views/components/Footer/footer.css

--- src/views/screens/Detail/detail.tsx
+++ src/views/screens/Detail/detail.tsx
@@ -142,6 +142,7 @@ export default function DetailScreen() {
         <RelatedProduct relatedProductsData={relatedProducts} loading={loading}/>
         </Row>
       </Container>
+      <FooterComponent/>
     </RootLayout>
   );
 }
