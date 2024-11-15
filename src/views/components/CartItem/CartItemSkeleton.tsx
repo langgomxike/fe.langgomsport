005fa452a4e7ad680441c1b77f9312ba176ccdb8
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-11-16 00:12:26 +0700

    edit skeleton of cart item, show warming add to cart in detail

--- src/views/components/CartItem/CartItemSkeleton.tsx
+++ src/views/components/CartItem/CartItemSkeleton.tsx
@@ -3,88 +3,79 @@ import Skeleton from "react-loading-skeleton";
 import { useMediaQuery } from "react-responsive";
 
 type CartItemSkeletonProps = {
-    limit: number;
+  limit: number;
 };
 
 export default function CartItemSkeleton({ limit }: CartItemSkeletonProps) {
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
-    return (
+  return (
+    <>
+      {Array.from({ length: limit }).map((_, index) => (
         <>
-        {
-            Array.from({ length: limit }).map(
-                (_, index) => (
-                 <>
-                     <tr className="item-cart">
-          {/* Product image */}
-          <td>
-          <Skeleton height={150}/>
-          </td>
-  
-          {/* Product name, price */}
-          {!isMobile ? (
-            <>
-              {/* Product name, price on destop */}
-              <td width={"33%"}>
-              <Skeleton height={70}/>
-              </td>
+          <tr className="item-cart">
+
+            {/* Product name, price */}
+            {!isMobile ? (
+              <>
+                {/* Product image */}
+                <td>
+                  <Skeleton height={150} />
+                </td>
+                {/* Product name, price on destop */}
+                <td width={"33%"}>
+                  <Skeleton height={70} />
+                </td>
+                <td>
+                  <Skeleton height={50} />
+                </td>
+              </>
+            ) : (
+              <>
+               {/* Product image */}
+               <td>
+                  <Skeleton height={100} />
+                </td>
+                {/* Product name, price on mobile */}
+                <td>
+                  <Skeleton height={80} />
+                </td>
+              </>
+            )}
+
+            {/* Input quatity and delete button */}
+            {!isMobile ? (
+              <>
+                {/* Input quantity on desktop */}
+                <td>
+                  <Skeleton height={50} />
+                </td>
+
+                {/* Total price on desktop */}
+                <td>
+                  <Skeleton height={50} />
+                </td>
+
+                {/* Button delete on desktop */}
+                <td>
+                  <div className="btn-delete">
+                    <FiTrash2 className="btn-delete-icon" />
+                  </div>
+                </td>
+              </>
+            ) : (
               <td>
-              <Skeleton height={50}/>
-              </td>
-            </>
-          ) : (
-            <>
-              {/* Product name, price on mobile */}
-              <td>
-              <Skeleton height={100}/>
-              </td>
-            </>
-          )}
-  
-          {/* Input quatity and delete button */}
-          {!isMobile ? (
-            <>
-              {/* Input quantity on desktop */}
-              <td>
-              <Skeleton height={50}/>
-              </td>
-  
-              {/* Total price on desktop */}
-              <td>
-              <Skeleton height={50}/>
-              </td>
-  
-              {/* Button delete on desktop */}
-              <td>
-                <div className="btn-delete">
+                {/* Input quantity on moblie */}
+                <Skeleton height={50} />
+                {/* Button delete on moblie */}
+                {/* <div className="btn-delete mt-2">
                   <FiTrash2 className="btn-delete-icon" />
-                </div>
+                </div> */}
               </td>
-            </>
-          ) : (
-            <td>
-              {/* Input quantity on moblie */}
-              <div className="item-cart-input-quantity">
-                <input
-                  type="number"
-                  value={1}
-                  min={1}
-                  inputMode="numeric"
-                  autoComplete="off"
-                />
-              </div>
-              {/* Button delete on moblie */}
-              <div className="btn-delete mt-2">
-                <FiTrash2 className="btn-delete-icon" />
-              </div>
-            </td>
-          )}
-        </tr>
-                 </>
-                )
-              )
-        }
-       
-      </>
-    );
+            )}
+          </tr>
+        </>
+      ))}
+    </>
+  );
 }
--- src/views/components/ProductDetail/ProductDetail.tsx
+++ src/views/components/ProductDetail/ProductDetail.tsx
@@ -41,6 +41,7 @@ export default function ProductInfo({
   const [selectedColor, setSelectedColor] = useState<string | null>(null);
   const [variantsSize, setVariantsSize] = useState<VariantSize[]>([]);
   const [order, setOrder] = useState<Order>();
+  const [showWarning, setShowWarning] = useState(false);
 
   // hendler
 
@@ -112,25 +113,30 @@ export default function ProductInfo({
             variant.size.id === selectedSize &&
             variant.color.color === selectedColor
         );
+        setShowWarning(false);
       } else if (selectedSize) {
         // Nếu không có màu, chỉ tìm theo size
         selectedVariant = detailData.variants?.find(
           (variant) => variant.size.id === selectedSize
         );
+        setShowWarning(false);
       } else if (selectedColor) {
         // Nếu không có size, chỉ tìm theo màu
         selectedVariant = detailData.variants?.find(
           (variant) => variant.color.color === selectedColor
         );
+        setShowWarning(false);
       } else {
         // Nếu không có gì chọn, chọn variant đầu tiên (hoặc thông báo lỗi nếu cần)
         console.log("Chưa chọn gì cả!");
+        setShowWarning(true);
       }
     }
     
 
     if (!selectedVariant) {
       console.warn("No valid variant found");
+      setShowWarning(true);
       return;
     }
 
@@ -184,6 +190,9 @@ export default function ProductInfo({
     console.log(detailData);
   }, [detailData]);
 
+  console.log("Show", showWarning);
+  
+
   // render
   return (
     <div>
@@ -224,13 +233,6 @@ export default function ProductInfo({
                 <span className="price-precent">(-{detailData.discount}%)</span>
               </>
             )}
-            {detailData.discount <= 0 && (
-              <>
-                <span className="product-price-main">
-                  {formatPrice(detailData.price)}
-                </span>
-              </>
-            )}
           </div>
 
           {/* Product sizes */}
@@ -252,7 +254,10 @@ export default function ProductInfo({
                   </div>
                 ))}
             </div>
-            {/* <span className="titleWarming">Vui lòng chọn kích thước</span> */}
+            {showWarning && selectedSize === -1 && (
+              <span className="titleWarming">Vui lòng chọn kích thước</span>
+            )}
+
           </div>
         </div>
       )}
