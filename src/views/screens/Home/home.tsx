775eb4f29d0e1f4462c33012bcf959d68ab1ab89
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2024-11-27 17:11:02 +0700

    create home structure

--- src/views/screens/Home/home.tsx
+++ src/views/screens/Home/home.tsx
@@ -1,10 +1,31 @@
 import FooterComponent from "../../components/Footer/Footer";
 import RootLayout from "../../layouts/RootLayout";
 
-export default function Home () {
-    return (
-        <RootLayout>
-            <h1>Trang chủ</h1>
-        </RootLayout>
-    )
+export default function Home() {
+  return (
+    <RootLayout>
+      <div className="container">
+        {/* Menu */}
+        <div className="menuContainer"></div>
+        
+        {/* Banners */}
+        <div className="bannerConainer"></div>
+
+        {/* Brands List*/}
+        <div className="brandsContainer"></div>
+
+        {/* New Product List */}
+        <div className="newProductList"></div>
+
+         {/* Sale Product List */}
+         <div className="saleProductList"></div>
+
+         {/* Collection */}
+         <div className="collectionContainer"></div>
+
+         {/* Policy notice  */}
+         <div className="policyNoticeContainer"></div>
+      </div>
+    </RootLayout>
+  );
 }
